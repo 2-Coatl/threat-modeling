a160e06de7922ec2bafcb12676c6f9eaaa8d079c
Coatl <121911012+2-Coatl@users.noreply.github.com>
2025-10-23 22:59:57 -0600

    Add Jest tests for the modular UI

---
+++ ui/src/modules/home/index.js
@@ -0,0 +1,3 @@
+export { default } from './HomeModule.jsx';
+export { default as homeReducer, selectHomeAnnouncement, rotateAnnouncement } from './state/homeSlice';
+export { default as useHomeAnnouncement } from './hooks/useHomeAnnouncement';
